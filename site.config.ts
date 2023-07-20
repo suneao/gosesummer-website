ff505ac449aad73abcb1debf2cc10a3b034cfe83
Gosesummer <51499066+suneao@users.noreply.github.com>
2023-07-20 10:43:06 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+  {
+       title: 'About',
+       pageId: '504ed4c9fb3b46c4ad7fb62b53fe3cde'
+     },
+     {
+       title: 'Contact',
+       pageId: 'cc184efb1e414c30be541a79f3ca1bc5'
+     }
+   ]
 })
