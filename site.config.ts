4ddd326945d0a3e77ef2471dc7f5720d848083f4
Gosesummer <51499066+suneao@users.noreply.github.com>
2023-07-24 15:33:36 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -28,7 +28,7 @@ export default siteConfig({
   // page-specific values will override these site-wide defaults
   defaultPageIcon: '💩',
   defaultPageCover: null,
-  defaultPageCoverPosition: 0.7,
+  defaultPageCoverPosition: 1,
 
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
